ea0bee736f2c496a92c7c83a6a030e570f8e2386
Saikat Dey <57017288+fanthomless@users.noreply.github.com>
2022-03-08 17:59:11 +0530

    stop rm from removing parent dir

--- nginx-setup.sh
+++ nginx-setup.sh
@@ -15,7 +15,7 @@ sudo ufw allow "Nginx Full"
 # sudo ufw enable # interactive, so can't be automated
 sudo ufw --force enable
 
-sudo rm -rf /etc/nginx/sites-enabled/
+sudo rm -rf /etc/nginx/sites-enabled/*
 #touch /etc/nginx/config.d/digonto.in.conf
 sudo curl -o digonto.in.config https://raw.githubusercontent.com/fanthomless/scripts/digonto.in.config
 sudo nginx -s reload
