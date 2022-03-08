ddfaf7e7335550b267182a6c7f4894616f78eb31
Saikat Dey <57017288+fanthomless@users.noreply.github.com>
2022-03-08 17:57:07 +0530

    changed ufw enable to prog mode from interactive mode

--- nginx-setup.sh
+++ nginx-setup.sh
@@ -12,8 +12,10 @@ sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
 sudo apt install -y nginx
 sudo ufw allow OpenSSH
 sudo ufw allow "Nginx Full"
+# sudo ufw enable # interactive, so can't be automated
+sudo ufw --force enable
 
-rm -rf /etc/nginx/sites-enabled/
+sudo rm -rf /etc/nginx/sites-enabled/
 #touch /etc/nginx/config.d/digonto.in.conf
 sudo curl -o digonto.in.config https://raw.githubusercontent.com/fanthomless/scripts/digonto.in.config
 sudo nginx -s reload
