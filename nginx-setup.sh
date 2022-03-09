4dad876c3932669b23b81bed843ddb23afc2ae07
Saikat Dey <57017288+fanthomless@users.noreply.github.com>
2022-03-09 20:05:02 +0530

    updated working directory

--- nginx-setup.sh
+++ nginx-setup.sh
@@ -2,7 +2,6 @@
 sudo apt update
 sudo apt -y upgrade
 
-
 # setup python
 sudo apt install -y python3-pip
 sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
@@ -15,7 +14,16 @@ sudo ufw allow "Nginx Full"
 # sudo ufw enable # interactive, so can't be automated
 sudo ufw --force enable
 
-sudo rm -rf /etc/nginx/sites-enabled/* #doesnt remove .dirs, use find or a regex to do that if req
+
+# change working directory 
+WORK_DIRECTORY="work-dir"
+
+cd ~
+mkdir -p $WORK_DIRECTORY
+cd $WORK_DIRECTORY
+
+
+sudo rm -rf /etc/nginx/sites-enabled/* # doesnt remove .dirs, use find or a regex to do that if required
 #touch /etc/nginx/config.d/digonto.in.conf
 sudo curl -o digonto.in.config https://raw.githubusercontent.com/fanthomless/scripts/digonto.in.config
 sudo nginx -s reload
