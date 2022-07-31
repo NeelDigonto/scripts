befd2106a98727d5de349740563db1f6797b6c05
Saikat Dey <57017288+notfathomless@users.noreply.github.com>
2022-08-01 01:21:19 +0530

    sudo required

--- nginx-nodejs-setup.sh
+++ nginx-nodejs-setup.sh
@@ -1,8 +1,8 @@
 sudo curl -o nginx-setup.sh https://raw.githubusercontent.com/fanthomless/scripts/main/nginx-setup.sh
 sudo curl -o nodejs-setup.sh https://raw.githubusercontent.com/fanthomless/scripts/main/nodejs-setup.sh
 
-chmod u=rx nginx-setup.sh
-chmod u=rx nodjes-setup.sh
+sudo chmod u=rx nginx-setup.sh
+sudo chmod u=rx nodjes-setup.sh
 
 ./nginx-setup.sh
 ./nodejs-setup.sh
