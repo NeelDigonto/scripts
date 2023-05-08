f243a584f93b78399fad498ec7f6177b4c40c219
Saikat Dey <57017288+notfathomless@users.noreply.github.com>
2023-05-09 00:33:22 +0530

    Bump version to 18

--- nodejs-setup.sh
+++ nodejs-setup.sh
@@ -1,9 +1,10 @@
 #Install and setup nodejs
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 source ~/.bashrc
-nvm install lts/gallium
-nvm use lts/gallium
+nvm install lts/hydrogen
+nvm use lts/hydrogen
 node -v
 
 npm install -g yarn
 npm install -g pm2
+npm install -g nodemon
